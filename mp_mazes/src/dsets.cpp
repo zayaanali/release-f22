c6cd753fb54518742940eced18033be74ae46da3
zayaanali <zayaanali02@gmail.com>
2022-10-31 15:17:53 -0500

    maze pt1 complete

--- mp_mazes/src/dsets.cpp
+++ mp_mazes/src/dsets.cpp
@@ -1,1 +1,39 @@
-/* Your code here! */
+
+
+#include "dsets.h"
+using namespace std;
+
+void DisjointSets::addelements(int num) {
+    int newsize = _elems.size() + num;
+    _elems.resize(newsize, -1);
+}
+
+int DisjointSets::find(int elem) {
+    if (_elems[elem]<0) {
+        return elem;
+    } else {
+        int rootElem = find(_elems[elem]);
+        _elems[elem] = rootElem;
+        return rootElem;
+    }
+}
+
+void DisjointSets::setunion(int a, int b) {
+    int rootA = find(a);
+    int rootB = find(b);
+    int newSize= _elems[rootA]+_elems[rootB];
+
+    if (_elems[rootA]<_elems[rootB]) { // rootB is larger
+        _elems[rootB] = rootA; // point rootB to rootA
+        _elems[rootA] = newSize;
+    } else { // rootA is larger (or same size)
+        _elems[rootA]=rootB; // point rootA to rootB
+        _elems[rootB]=newSize;
+    }   
+}
+
+int DisjointSets::size(int elem) {
+    int root=find(elem);
+    return _elems[root]*-1;
+
+}
--- mp_mazes/src/dsets.h
+++ mp_mazes/src/dsets.h
@@ -1,1 +1,16 @@
 /* Your code here! */
+#pragma once
+#include<vector>
+using std::vector;
+/* Your code here! */
+class DisjointSets {
+    public:
+        void addelements (int num);
+        int find(int elem);
+        void setunion(int a, int b);
+        int size(int elem);
+    
+    private:
+        vector<int> _elems;
+
+};
